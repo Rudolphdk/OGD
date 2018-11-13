c505859d8d771123885f4ec51f5e061b53f6ee27
Rudolph de Kock <rudolphk@lightstone.co.za>
2018-11-13 14:15:27 +0200

    Added facebook login

--- OGDMovies.Web/App_Start/BundleConfig.cs
+++ OGDMovies.Web/App_Start/BundleConfig.cs
@@ -22,6 +22,9 @@ namespace OGDMovies.Web
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/facebook").Include(
+                      "~/Scripts/js/facebook.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
